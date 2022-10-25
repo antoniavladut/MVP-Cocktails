25d2b1528ad22c9583210b4ee1c037cf36a5c9c1
Freya McLeish <freyamcleish@gmail.com>
2022-10-25 16:37:08 +0100

    backend finished for day 25th October

--- my-express-app/routes/recipes.js
+++ my-express-app/routes/recipes.js
@@ -61,48 +61,24 @@ router.get("/:id", async function (req, res, next) {
   }
 });
 
-// /* Get recipe by ingredient */
-// router.get("/search", async function (req, res, next) {
-//   // create ingredient for searching
-
-//   let recipeIngredient = req.params.ingredients;
-//   try {
-//     // create results if cocktail ingredient matches searched ingredient
-//     let results = await db(
-//       `SELECT * FROM cocktails WHERE ingredients LIKE '%${recipeIngredient}%'`
-//     );
-//     let cocktails = results.data;
-//     if (cocktails.length === 0) {
-//       // cocktail array empty, no cocktail found
-//       res.status(404).send({ error: "Cocktail not found" });
-//     } else {
-//       // return located cocktail as object
-//       res.send(cocktails);
-//     }
-//   } catch (err) {
-//     // server error
-//     res.status(500).send(err);
-//   }
-// });
-
-// /*Insert new recipe into DB */
-// router.post("/", async function (req, res, next) {
-//   let { title, method, glass, ice, garnish, ingredients, instructions } =
-//     req.body;
-//   // sql command line for inserting cocktail (as completed in initial set up)
-//   let sql = `INSERT INTO cocktails (title, method, glass, ice, garnish, ingredients, instructions )
-//     VALUES ('${title}', '${method}', '${glass}', '${ice}', '${garnish}', '${ingredients}', '${instructions}')`;
-//   // adding new cocktail
-//   try {
-//     await db(sql); // add cocktail when function called
-//     let list = await db(`SELECT * FROM cocktails`); // return whole cocktail list
-//     let cocktails = list.data; // add managable & comprehensive variable
-//     res.status(201).send(cocktails); // send updated array
-//     // server error
-//   } catch (err) {
-//     res.status(500).send(err.message);
-//   }
-// });
+/*Insert new recipe into DB */
+router.post("/", async function (req, res, next) {
+  let { title, method, glass, ice, garnish, ingredients, instructions } =
+    req.body;
+  // sql command line for inserting cocktail (as completed in initial set up)
+  let sql = `INSERT INTO cocktails (title, method, glass, ice, garnish, ingredients, instructions )
+    VALUES ('${title}', '${method}', '${glass}', '${ice}', '${garnish}', '${ingredients}', '${instructions}')`;
+  // adding new cocktail
+  try {
+    await db(sql); // add cocktail when function called
+    let list = await db(`SELECT * FROM cocktails`); // return whole cocktail list
+    let cocktails = list.data; // add managable & comprehensive variable
+    res.status(201).send(cocktails); // send updated array
+    // server error
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
 
 /* Delete a cocktail from the DB */
 router.delete("/:id", async function (req, res, next) {
@@ -130,12 +106,3 @@ router.delete("/:id", async function (req, res, next) {
 });
 
 module.exports = router;
-
-/* GET recipes list. */
-router.get("/", function (req, res, next) {
-  db("SELECT * FROM cocktails;")
-    .then((results) => {
-      res.send(results.data);
-    })
-    .catch((err) => res.status(500).send(err));
-});
