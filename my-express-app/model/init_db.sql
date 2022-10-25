a248e193498ec9adc094c2949c45db9d7615ce58
Freya McLeish <freyamcleish@gmail.com>
2022-10-25 12:40:43 +0100

    initial DB added (attempt 2)

--- my-express-app/model/init_db.sql
+++ my-express-app/model/init_db.sql
@@ -1,16 +1,7 @@
-require("dotenv").config(); 
-const mysql = require("mysql"); 
-const fs = require("fs");  // new
 
-console.log("Connected!"); 
- 
-let sql = fs.readFileSync(__dirname+"/init_db.sql").toString();  // new 
-con.query(sql, function(err, result) {
-
-DROP TABLE IF EXISTS cocktails; 
- 
-CREATE TABLE cocktails ( 
+ CREATE TABLE IF NOT EXISTS cocktails ( 
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+    title VARCHAR(100), 
     method VARCHAR(100), 
     glass VARCHAR(100),
     ice VARCHAR(100),
@@ -20,5 +11,5 @@ CREATE TABLE cocktails (
 
 ); 
  
-INSERT INTO cocktails (title, method, glass, ice, garnish, ingredients, instructions)  
-    VALUES ('Cosmopolitan', 'Shake', 'Martini', 'Cubed', 'Orange Peel', '2cl Vodka 2cl Cointreau 1cl Lime Juice 3cl Cranberry Juice', '1. Chill glass with ice, and add another handful to the shaker 2. Add all ingredients to shaker and shake hard 3. Empty ice from glasses and strain drink into chilled glass 4. Add orange peel twist 5. Serve!'), ('Cuba Libre' 'Build', 'Highball', 'Cubed', 'Lime Wedge', '4cl Light Rum 1cl Lime Juice 8cl Cola', '1. Add ice to glass 2. Pour lime juice and rum in glass, give it a stir 3. Pour cola on top 4. Add lime wedge garnish 5. Serve!'); 
+-- INSERT INTO cocktails (title, method, glass, ice, garnish, ingredients, instructions)  
+--     VALUES ('Cosmopolitan', 'Shake', 'Martini', 'Cubed', 'Orange Peel', '2cl Vodka 2cl Cointreau 1cl Lime Juice 3cl Cranberry Juice', '1. Chill glass with ice, and add another handful to the shaker 2. Add all ingredients to shaker and shake hard 3. Empty ice from glasses and strain drink into chilled glass 4. Add orange peel twist 5. Serve!'), ('Cuba Libre', 'Build', 'Highball', 'Cubed', 'Lime Wedge', '4cl Light Rum 1cl Lime Juice 8cl Cola', '1. Add ice to glass 2. Pour lime juice and rum in glass, give it a stir 3. Pour cola on top 4. Add lime wedge garnish 5. Serve!'); 
