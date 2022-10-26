e87e07f2cd60a081f40613c5a94e6aafea794673
Freya McLeish <freyamcleish@gmail.com>
2022-10-26 17:05:08 +0100

    add a cocktail working & directs back to recipe page

--- my-react-app/src/pages/AddCocktailForm.js
+++ my-react-app/src/pages/AddCocktailForm.js
@@ -1,7 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./AddCocktailForm.css";
 
 function AddCocktailForm() {
-  return <div>Add A Cocktail</div>;
+  const nav = useNavigate();
+
+  const [fields, setFields] = useState({
+    title: "",
+    method: "",
+    glass: "",
+    ice: "",
+    garnish: "",
+    ingredients: "",
+    instructions: "",
+  });
+
+  async function addCocktail() {
+    try {
+      // create response - fetch data, method post
+      let response = await fetch("/recipes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(fields),
+      });
+      //if response fetch works
+      if (response.ok) {
+        nav("/recipes");
+      } else {
+        console.log(`Server Error: ${response.status} ${response.statusText}`);
+      }
+    } catch (err) {
+      console.log(`Network Error: ${err.message}`);
+    }
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    addCocktail();
+  }
+
+  const onChange = (fieldName) => (e) => {
+    return setFields({ ...fields, [fieldName]: e.target.value });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        <div>Title</div>
+        <input type="text" value={fields.title} onChange={onChange("title")} />
+      </label>
+      <label>
+        <div>Method</div>
+        <input type="text" value={fields.method} onChange={onChange("method")} />
+      </label>
+      <label>
+        <div>Glass</div>
+        <input type="text" value={fields.glass} onChange={onChange("glass")} />
+      </label>
+      <label>
+        <div>Ice</div>
+        <input type="text" value={fields.ice} onChange={onChange("ice")} />
+      </label>
+      <label>
+        <div>Garnish</div>
+        <input type="text" value={fields.garnish} onChange={onChange("garnish")} />
+      </label>
+      <label>
+        <div>Ingredients</div>
+        <input type="text" value={fields.ingredients} onChange={onChange("ingredients")} />
+      </label>
+      <label>
+        <div>Instructions</div>
+        <input type="text" value={fields.instructions} onChange={onChange("instructions")} />
+      </label>
+      <button type="submit">Add Cocktail</button>
+    </form>
+  );
 }
 
 export default AddCocktailForm;
